8c0c4f59573bc1ceb740d1a447605e3403c56b8f
volynkina <bzzz18@yandex.ru>
2023-01-20 19:47:51 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+        int [] weights = new int[12];
+        weights [0] = 1;
+        weights [1] = 2;
+        weights [2] = 3;
+        for (int i = 0; i < 12; i++) {
+            System.out.println(weights [i]);
+        }
+        System.out.println();
+        double [] a = {1.57, 7.654, 9.986,0,0,0,0,0,0,0,0,0 };
+        a [0] = 1.57;
+        for (int i = 0; i < 12; i++) {
+            System.out.println(a [i]);
+        }
+        System.out.println();
+        int [] beauty = {3,5,7,9,0,0,0};
+        beauty [0] = 3;
+        for (int i = 0; i < 7; i++) {
+            System.out.println(beauty[i]);
+        }
+   }
 }
