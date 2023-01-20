39626f7512e3a589597f48d5527aac0ba24240dd
volynkina <bzzz18@yandex.ru>
2023-01-20 21:06:49 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,37 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int [] weights = new int[12];
-        weights [0] = 1;
-        weights [1] = 2;
-        weights [2] = 3;
+        int[] weights = new int[12];
+        weights[0] = 1;
+        weights[1] = 2;
+        weights[2] = 3;
         for (int i = 0; i < 12; i++) {
-            System.out.println(weights [i]);
+            System.out.println(weights[i]);
         }
         System.out.println();
-        double [] a = {1.57, 7.654, 9.986,0,0,0,0,0,0,0,0,0 };
-        a [0] = 1.57;
+        double[] a = {1.57, 7.654, 9.986, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        a[0] = 1.57;
         for (int i = 0; i < 12; i++) {
-            System.out.println(a [i]);
+            System.out.println(a[i]);
         }
         System.out.println();
-        int [] beauty = {3,5,7,9,0,0,0};
-        beauty [0] = 3;
+        int[] beauty = {3, 5, 7, 9, 0, 0, 0};
+        beauty[0] = 3;
         for (int i = 0; i < 7; i++) {
             System.out.println(beauty[i]);
         }
-   }
+        System.out.println();
+        for (int i = 0; i < 12; i++) {
+            System.out.print(weights[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 12; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 7; i++) {
+            System.out.print(beauty[i] + " ");
+        }
+
+    }
 }
