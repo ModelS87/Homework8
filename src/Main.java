800eaea0df5effd9cd0d5e7c15e00c86d2857e46
volynkina <bzzz18@yandex.ru>
2023-01-21 17:36:18 +0500

    task 4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -71,6 +73,16 @@ public class Main {
             }
             System.out.print(beauty[i] + ", ");
         }
+        System.out.println();
+
+        int[] arr = {1,2,3};
+        for (int i = 0; i < arr.length; i++ ) {
+            if (arr[i] % 2 != 0 ) {
+                arr[i] = arr[i] + 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
     }
     }
 
